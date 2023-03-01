df07b9ca244d318195715e371d1e8fec44823c31
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-03-01 13:38:55 +0200

    Update MoviesDetalis.jsx

--- src/pages/MoviesDetalis.jsx
+++ src/pages/MoviesDetalis.jsx
@@ -21,7 +21,7 @@ const MoviesDetalis = () => {
   const movieServices = new MovieServices();
   console.log({ from: location });
   console.log(location);
-  const backLinkHref = location.state?.from ?? location.from;
+  const backLinkHref = location.state?.from ?? '/movies';
 
   useEffect(() => {
     onRequest(movieId);
